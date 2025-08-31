af84914e906855293e03ff975654d5504328c5f1
Marek Skórkowski <marek@constellationsoft.com>
2025-09-01 01:56:56 +0200

    Fix flake.nix and add flake.lock

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         packages = nixpkgs.legacyPackages.${system};
         rust-toolchain = with fenix.packages.${system}; fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-IyelwCDfJ/ASxSMH6tKFO17DJybfI9Hl05i0jIXK898=";
+          sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
         };
       in {
         devShell = packages.mkShell rec {
@@ -33,62 +33,3 @@
       }
     );
 }
-# {
-#   description = "A basic Rust devshell for NixOS users developing Leptos";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#     rust-overlay.url = "github:oxalica/rust-overlay";
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-
-#   outputs =
-#     {
-#       nixpkgs,
-#       rust-overlay,
-#       flake-utils,
-#       ...
-#     }:
-#     flake-utils.lib.eachDefaultSystem (
-#       system:
-#       let
-#         overlays = [ (import rust-overlay) ];
-#         pkgs = import nixpkgs {
-#           inherit system overlays;
-#         };
-#       in
-#       with pkgs;
-#       {
-#         devShells.default = mkShell {
-#           buildInputs =
-#             [
-#               gcc
-#               glib
-#               openssl
-#               pkg-config
-#               cacert
-#               cargo-make
-#               trunk
-#               (rust-bin.selectLatestNightlyWith (
-#                 toolchain:
-#                 toolchain.default.override {
-#                   extensions = [
-#                     "rust-src"
-#                     "rust-analyzer"
-#                   ];
-#                   targets = [ "wasm32-unknown-unknown" ];
-#                 }
-#               ))
-#               tailwindcss_4
-#               rust-analyzer
-#               clang
-#             ]
-#             ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
-#               darwin.apple_sdk.frameworks.SystemConfiguration
-#             ];
-
-#           shellHook = '''';
-#         };
-#       }
-#     );
-# }
