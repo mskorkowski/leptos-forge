659bd38415361c3998d91d4fbaded3d0663de8d6
Marek Skórkowski <marek@constellationsoft.com>
2025-09-26 01:56:52 +0200

    [#3-documentation] Fixed some small issues in the rest of documentation

--- site/src/rust/main/stories/setup/resources.rs
+++ site/src/rust/main/stories/setup/resources.rs
@@ -57,32 +57,39 @@ Providing the resource has three steps.
    
 ### Using the resources
 
-To use the resources there is an additional configuration still to be made. In the application's `Cargo.toml`  you need to add
-`package.metadata.cargo_resources.resource_root`. This is the path to directory relative to the `Cargo.toml` where the resources will be placed.
+To use the resources there is an additional configuration still to be made. In
+the application's `Cargo.toml` you need to add `package.metadata.cargo_resources.resource_root`. 
+This is the path to directory relative to the `Cargo.toml` where the resources 
+will be placed.
 
 ```toml
 [package.metadata.cargo_resources]
 resource_root = "target/resources"
 ```
 
-This gives the final path of the resources to be concatenation of the `resource_root` and the `output_path` of the specific resource. In the case of `logo.png` file
-used in examples in this section it would be `targe/resources/my-crate/images/logo.png`.
+This gives the final path of the resources to be concatenation of the `resource_root`
+and the `output_path` of the specific resource. In the case of `logo.png` file 
+used in examples in this section it would be `target/resources/my-crate/images/logo.png`.
 
 > [!TIP]
 >
-> Writing the `leptos_forge` we found it useful to follow the convention where `output_path` for a resource includes a name of a create to which it belongs.
-> This makes it easier to manage the resources since you don't override the files by accident.
+> Writing the `leptos_forge` we found it useful to follow the convention where 
+> `output_path` for a resource includes a name of a crate to which it belongs.
+> This makes it easier to manage the resources since you don't override the files
+> by accident.
 
 ### Running the application
 
-Before you run the application you need to run the `cargo resources` command or use the `build.rs` script. There are two ways to run the command for your `leptos_forge` based
-project.
+Before you run the application you need to run the `cargo resources` command or
+use the `build.rs` script. There are two ways to run the command for your
+`leptos_forge` based project.
 
 1. Let it be part of your build script (as seen in the setup)
 2. Run it from using `trunk`
 3. Manual running `cargo resources`
 
-Both options have their advantages and disadvantages. Comparison is presented in the table below
+Both options have their advantages and disadvantages. Comparison is presented in
+the table below
 
 |                        | Trunk                                       | Build script                                | Manual
 |:-----------------------|:--------------------------------------------|:--------------------------------------------|:--------------------------------------------|
@@ -90,7 +97,8 @@ Both options have their advantages and disadvantages. Comparison is presented in
 | nix                    | `cargo_resources` is both a lib and cli application. Unfortunate part is that it's not packaged for `nix`. | Since it's a build script, `cargo-resources` will be build as a lib and linked to the build script which makes it trivial to use. | The same issue as trunk has. |
 | Other build configurations | This works only for trunk. Any other build setup will fail | This works for any setup which runs a `cargo build` internally. | You must not forget to run it |
 
-If you followed the [create project guide](/guides/create_project) then you have a build script configured already.
+If you followed the [create project guide](/guides/create_project) then you have
+a build script configured already.
 
 #### Using `trunk` to bundle the resources
 
@@ -105,9 +113,11 @@ command_arguments = ["resources"]
 
 #### Using build script
 
-If you followed the [create project](/guides/create_project) this is a configuration which was suggested to you.
+If you followed the [create project](/guides/create_project), this configuration
+was suggested to you.
 
-To make the build script approach work you need to make following changes in the `Cargo.toml`
+To make the build script approach work, you need to make the following changes
+in the `Cargo.toml`:
 
 ```toml
 [build-dependencies]
@@ -117,7 +127,7 @@ cargo-resources="1.1.6"
 
 This will add the necessary dependencies for the build script approach.
 
-Now beside the root of your crate you need to add the `build.rs` file
+Now beside the root of your crate you need to add the `build.rs` file:
 
 ```rust
 use std::env::current_dir;
@@ -150,15 +160,18 @@ impl Section for Resources {
 const TAILWIND: &str = r############"
 # Tailwind
 
-`leptos_forge` uses Tailwind 4 to manage the css styles.
+`leptos_forge` uses Tailwind 4 to manage the CSS styles.
 
-To use the Tailwind within the `leptos_forge` based application you need to adjust the Tailwind `main.css` file to include the necessary files and
-do small changes to the `index.html` file used by the `trunk`.
+To use the Tailwind within the `leptos_forge` based application you need to
+adjust the Tailwind `main.css` file to include the necessary files and make
+small changes to the `index.html` file used by the `trunk`.
 
 ## Setting up `cargo-resources`
 
-I assume you have `cargo-resources` set and running. If not you can follow either [setup](/guides) or [resources](/guides/resources) chapter. For the
-rest of this chapter I will assume that in your `Cargo.toml` you have the following configuration
+I assume you have `cargo-resources` set up and running. If not you can follow
+either [setup](/guides) or [resources](/guides/resources) chapter. For the rest
+of this chapter I will assume that in your `Cargo.toml` you have the following
+configuration
 
 ```toml
 [package.metadata.cargo_resources]
@@ -167,8 +180,9 @@ resource_root = "target/resources"
 
 ## Setting up Tailwind
 
-I assume you have installed Tailwind and have the `assets/css/main.css` file in your project as entry point for your Tailwind. If your `main.css`
-is somewhere else you will need to adjust the paths accordingly.
+I assume you have installed Tailwind and have the `assets/css/main.css` file in
+your project as the entry point for your Tailwind. If your `main.css` is
+somewhere else you will need to adjust the paths accordingly.
 
 `leptos_forge` provides two files in for you to include in the `main.css` file. 
 
@@ -184,7 +198,8 @@ To integrate `leptos_forge` with your project's Tailwind setup you need to impor
 
 ## Setting up trunk
 
-Trunk has builtin integration with Tailwind. Your `index.html` file needs to be slightly adjusted compared to the [create project](/guides/create_project)
+Trunk has built-in integration with Tailwind. Your `index.html` file needs to
+be slightly adjusted compared to the [create project](/guides/create_project)
 
 ```html
 <!doctype html>
