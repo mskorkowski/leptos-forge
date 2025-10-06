40413727c3cc26d26cb335649ec4c69626525dfa
Marek Skórkowski <marek@constellationsoft.com>
2025-10-07 01:17:53 +0200

    [#65 - Improve error reporting when tailwind fails]
    When tailwind errors, then we will print the stdout and stderr properly (and also fail the build).

--- forge/src/rust/build/build.rs
+++ forge/src/rust/build/build.rs
@@ -9,23 +9,59 @@
 //! 1. Monitor the changes in the `src/css/main.css` file and rebuild the project when there are changes
 
 use std::process::Command;
-
+use build_print::{println, error, info};
 /// Description in case of tailwind execution failure
-const TAILWIND_FAILURE: &str = r############"Failed to build css for leptos-forge.
+const TAILWIND_FAILURE: &str = "Build script can't find the tailwindcss cli. Please check your tailwind installation";
 
-1. Please check your tailwind installation
-"############;
+/// Simple helper function to print the multiline string to the cargo output
+fn error<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            error!("{line}");
+    }
+}
+
+/// Simple helper function to print the multiline string to the cargo output
+fn info<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            info!("{line}");
+    }
+    
+}
+
+/// Simple helper function to print a normal multiline test to the cargo output
+fn println<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            println!("{line}");
+    }
+}
 
 /// Entry point for the build script.
 fn main() {
-    println!("cargo::rerun-if-changed=src/css/main");
-    println!("cargo::rerun-if-changed=src/resources/logo");
+    info("leptos_forge: Running build script!");
+    std::println!("cargo::rerun-if-changed=src/css/main");
+    std::println!("cargo::rerun-if-changed=src/resources/logo");
 
-    Command::new("tailwindcss")
+
+    let output = Command::new("tailwindcss")
         .args(vec![
             "--input", "src/css/main/main.css",
             "--output", "src/resources/generated/leptos-forge.css"
         ])
         .output()
         .expect(TAILWIND_FAILURE);
+
+    if !output.status.success() {
+        error(format!("Tailwind Stopped with an {}", output.status));
+        error("");
+        error("--------[ STDOUT ]------------------------");
+        error("");
+        println(String::from_utf8_lossy(&output.stdout));
+        error("");
+        error("--------[ STDERR ]------------------------");
+        error("");
+        error(String::from_utf8_lossy(&output.stderr));
+
+    }
+
+
 }
