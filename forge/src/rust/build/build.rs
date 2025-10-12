f7da4a7d39acea062b905df2081be2969e57c502
Marek Skórkowski <marek@constellationsoft.com>
2025-10-12 12:03:48 +0200

    [#72 - Improve the reliability of tailwind and trunk integration] We moved css generation to the cargo build scripts, and hack around the `cargo-resources` in `leptos_forge`.

--- forge/src/rust/build/build.rs
+++ forge/src/rust/build/build.rs
@@ -8,30 +8,49 @@
 //! 
 //! 1. Monitor the changes in the `src/css/main.css` file and rebuild the project when there are changes
 
+use std::thread::sleep;
+use std::time::Duration;
+use std::{env::current_dir};
 use std::process::Command;
-use build_print::{println, error, info};
+use cargo_metadata::camino::Utf8PathBuf;
+use cargo_resources::collate_resources;
+use build_print::{println, error, info, warn};
+use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 /// Description in case of tailwind execution failure
 const TAILWIND_FAILURE: &str = "Build script can't find the tailwindcss cli. Please check your tailwind installation";
 
-/// Simple helper function to print the multiline string to the cargo output
+/// Tag to prefix the output lines so we know from where it comes from
+const CRATE_TAG: &str = "forge";
+
+/// Simple helper function to print the multiline error message to the cargo output
 fn error<S: ToString>(s: S) {
     for line in s.to_string().split("\n") {
-            error!("{line}");
+            error!("[{CRATE_TAG}] {line}");
     }
 }
 
-/// Simple helper function to print the multiline string to the cargo output
+/// Simple helper function to print the multiline warning message to the cargo output
+fn warn<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+        warn!("[{CRATE_TAG}] {line}");
+    }
+}
+
+/// Simple helper function to print the multiline information message to the cargo output
 fn info<S: ToString>(s: S) {
     for line in s.to_string().split("\n") {
-            info!("{line}");
+            info!("[{CRATE_TAG}] {line}");
     }
     
 }
 
-/// Simple helper function to print a normal multiline test to the cargo output
+/// Simple helper function to print a normal multiline text to the cargo output
 fn println<S: ToString>(s: S) {
     for line in s.to_string().split("\n") {
-            println!("{line}");
+            println!("[{CRATE_TAG}] {line}");
     }
 }
 
@@ -40,12 +59,72 @@ fn main() {
     info("leptos_forge: Running build script!");
     std::println!("cargo::rerun-if-changed=src/css/main");
     std::println!("cargo::rerun-if-changed=src/resources/logo");
+    std::println!("cargo::rerun-if-changed=src/resources/generated");
 
+    // # cargo-resources work around
+    //
+    // # Current state
+    //
+    // cargo-resources when collates resources requires all of the resources
+    // to be available before collating
+    //
+    // The problem is when downstream crate needs to provide a resources based on
+    // upstream resources.
+    //
+    // To generate the resource crate needs to collate the resources. To collate
+    // the resource the generated resource must be present... And we have a loop
+    //
+    // # Workaround
+    //
+    // Create an empty file (if local resources are not present) and always 
+    // collate twice (first for the seeding) and second to update the just generated
+    // resources provided by this crate
+
+    // Creating an empty file
+    let generated_files = vec![
+        "target/resources/leptos_forge/main.css"
+    ];
+
+    for file in generated_files {
+        let path = Path::new(file);
+        if !path.exists() {
+            let Ok(mut f) = OpenOptions::new().
+                create_new(true).
+                write(true).
+                open(file) else {
+                    warn(format!("File '{file}' already exists"));
+                    continue;
+                };
+
+            let _ = writeln!(f, "  ");
+            let _ = f.sync_all();
+        }
+    }
+
+    let cwd = current_dir().unwrap();
+    let manifest_file = Utf8PathBuf::from_path_buf(cwd).unwrap().join("Cargo.toml");
+    let mut cnt = 0;
+
+    // Collate resources from the crate's dependencies so we can generate our 
+    // own stuff
+    while let Err(e) = collate_resources(&manifest_file) {
+        if cnt > 100 {
+            error("It was not possible to bundle the resources");
+            return;
+        }
+        
+        warn("Failed to bundle the crate");
+        println(format!("\n\ncargo-resources returned an error:\n\n{e}\n\n"));
+        sleep(Duration::from_millis(100));
+        warn("Retrying...");
+        cnt += 1;
+    }
 
     let output = Command::new("tailwindcss")
         .args(vec![
             "--input", "src/css/main/main.css",
-            "--output", "src/resources/generated/leptos-forge.css"
+            "--output", "target/resources/leptos_forge/main.css",
+            "--optimize"
         ])
         .output()
         .expect(TAILWIND_FAILURE);
@@ -62,6 +141,22 @@ fn main() {
         error(String::from_utf8_lossy(&output.stderr));
 
     }
+    else {
+        info("Tailwind run successfully");
+    }
 
+    // Rerun collate resource so we provide a proper thing downstream
+    while let Err(e) = collate_resources(&manifest_file) {
+        if cnt > 100 {
+            error("It was not possible to bundle the resources");
+            return;
+        }
+        
+        warn("Failed to bundle the crate");
+        println(format!("\n\ncargo-resources returned an error:\n\n{e}\n\n"));
+        sleep(Duration::from_millis(100));
+        warn("Retrying...");
+        cnt += 1;
+    }
 
 }
--- site/src/rust/build/build.rs
+++ site/src/rust/build/build.rs
@@ -5,17 +5,69 @@
 
 
 use std::env::current_dir;
+use std::process::Command;
 use cargo_metadata::camino::Utf8PathBuf;
 use cargo_resources::collate_resources;
+use build_print::{println, error, info};
+
+/// Description in case of tailwind execution failure
+const TAILWIND_FAILURE: &str = "Build script can't find the tailwindcss cli. Please check your tailwind installation";
+
+/// Simple helper function to print the multiline string to the cargo output
+fn error<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            error!("[site] {line}");
+    }
+}
+
+/// Simple helper function to print the multiline string to the cargo output
+fn info<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            info!("[site] {line}") ;
+    }
+    
+}
+
+/// Simple helper function to print a normal multiline test to the cargo output
+fn println<S: ToString>(s: S) {
+    for line in s.to_string().split("\n") {
+            println!("[site] {line}");
+    }
+}
 
 fn main() {
-    println!("cargo::rerun-if-changed=src/css/main");
-    println!("cargo::rerun-if-changed=src/js/loader");
-    println!("cargo::rerun-if-changed=src/resources");
+    std::println!("cargo::rerun-if-changed=src/css/main");
+    std::println!("cargo::rerun-if-changed=src/js/loader");
+    std::println!("cargo::rerun-if-changed=src/resources");
 
     let cwd = current_dir().unwrap();
     let manifest_file = Utf8PathBuf::from_path_buf(cwd).unwrap().join("Cargo.toml");
 
     // Collate resources from the crate's dependencies.
-    collate_resources(&manifest_file).expect("There was an error during bundling of the resources");
+    collate_resources(&manifest_file).expect("[site] There was an error during bundling of the resources");
+
+    let output = Command::new("tailwindcss")
+        .args(vec![
+            "--input", "src/css/main/main.css",
+            "--output", "target/resources/leptos_forge_site/main.css",
+            "--optimize"
+        ])
+        .output()
+        .expect(TAILWIND_FAILURE);
+
+    if !output.status.success() {
+        error(format!("Tailwind Stopped with an {}", output.status));
+        error("");
+        error("--------[ STDOUT ]------------------------");
+        error("");
+        println(String::from_utf8_lossy(&output.stdout));
+        error("");
+        error("--------[ STDERR ]------------------------");
+        error("");
+        error(String::from_utf8_lossy(&output.stderr));
+
+    }
+    else {
+        info("[SITE] Tailwind run successfully");
+    }
 }
