f1f82b522eced037b5ea429c0a56e5948e23ee1b
Marek Skórkowski <marek@constellationsoft.com>
2025-10-15 16:53:31 +0200

    [chore] Update the nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,59 @@
 {
+  description = "A basic Rust devshell for NixOS users developing Leptos";
+
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, fenix, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        packages = nixpkgs.legacyPackages.${system};
-        rust-toolchain = with fenix.packages.${system}; fromToolchainFile {
-          file = ./rust-toolchain.toml;
-          sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-      in {
-        devShell = packages.mkShell rec {
-          buildInputs = with packages; [
-            clang
-            cargo-shear
-            lld
-            mold-wrapped
-            pkg-config
-            rust-analyzer
-            rust-toolchain
-            tailwindcss_4
-            trunk
-          ];
-          LD_LIBRARY_PATH = packages.lib.makeLibraryPath buildInputs;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs =
+            [
+              # gcc
+              # glib
+              # openssl
+              pkg-config
+              # cacert
+              # cargo-make
+              trunk
+              tailwindcss_4
+              # clang
+              # lld
+              cargo-shear
+              (rust-bin.selectLatestNightlyWith (
+                toolchain:
+                toolchain.default.override {
+                  extensions = [
+                    "rust-src"
+                    "rust-analyzer"
+                  ];
+                  targets = [ "wasm32-unknown-unknown" ];
+                }
+              ))
+            ]
+            ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
+
+          shellHook = '''';
         };
       }
     );
